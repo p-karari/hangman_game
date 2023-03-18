5bf2795b3e7b281fd2086e4fb1cb276e976f37b5
p-karari <kararikelvin2002@gmail.com>
2023-03-18 21:33:26 +0300

    updates the logic of the game ,cleaner code

--- JS/app.js
+++ JS/app.js
@@ -5,62 +5,83 @@ const words = ["abruptly", "avenue", "baffled", "beacon", "cautious", "chase", "
 
 const buttons = document.querySelectorAll(".btn")
 const text = document.querySelectorAll(".txt")
-const player_guess = document.getElementById("input_box")
-// console.log(player_guess)
+const player_guess = document.getElementById('input_box').value
+console.log(player_guess)
+    let random_word 
+function random_word_selector(){
+     let i = Math.floor((Math.random() * 50))
+    random_word =  words[i]
 
-let random_word 
-// console.log(random_word)
-let i = Math.floor((Math.random() * 50))
-random_word =  words[i]
-// console.log(shuffle)
-function word_shuffle() {
+}
     
-    
+function word_shuffle() { 
+    text[1].style.textTransform = "lowerCase"
+   random_word_selector()
     const shuffle = random_word.split("")
    for (let k = 0; k < shuffle.length;k++ ) {
     let a = Math.floor((Math.random() * shuffle.length))
     let b = Math.floor((Math.random() * (shuffle.length - 1)))
     let tempolary_place_holder = shuffle[b]
     
-    // console.log("before swap :   " +shuffle[b] + "  |  " + shuffle[a])
-    
     shuffle[b] = shuffle[a]
     shuffle[a] = tempolary_place_holder
-    
-    // console.log("after swap :    " +shuffle[b] + "  |  " + shuffle[a])
-
-    // console.log("iteration number   " +k )
-    
     let randomised_word = shuffle.join("")
-// console.log(randomised_word)
-
-    
     text[1].textContent = randomised_word
-
-    
 } 
 }
-
-
-// console.log(randomised_word)
-
-
-// console.log(newGame)
 buttons[0].onclick = function() {
-    word_shuffle()
-    // alert("clicked")
+    word_shuffle() 
 }
 
-let guess_value = player_guess.value
-console.log(guess_value)
+let right_guess_message = "~~hooray!! you saved mr.Boogie Man"
+let wrong_guess_message = "~~oops!! wrong guess ,try again~~"
+let final_message = "~~couldn't save him , better luck next time~~"
 
-function confirm() {
-    let right_guess_message = "yipee , you gessed it right"
-    if (guess_value === random_word) {
+function confirm_guess() {
+    if (player_guess === random_word) {
         text[2].textContent = right_guess_message
     }
+    else {
+        for (let guess_count = 0;guess_count < 8;guess_count++) {
+            if(guess_count = 1){
+                Image.style.backgroundImage = ""
+                text[2].textContent = wrong_guess_message
+            }
+            else if(guess_count = 2) {
+                Image.style.backgroundImage = ""
+                text[2].textContent = wrong_guess_message
+            }
+            else if(guess_count = 3) {
+                Image.style.backgroundImage = ""
+                text[2].textContent = wrong_guess_message
+            }
+            else if(guess_count = 4) {
+                Image.style.backgroundImage = ""
+                text[2].textContent = wrong_guess_message
+            }
+            else if(guess_count = 5) {
+                Image.style.backgroundImage = ""
+                text[2].textContent = wrong_guess_message
+            }
+            else if(guess_count = 6) {
+                Image.style.backgroundImage = ""
+                text[2].textContent = wrong_guess_message
+            }
+            else if(guess_count = 7) {
+                Image.style.backgroundImage = ""
+                text[2].textContent = final_message
+            }
+            else {
+                Image.style.backgroundImage = ""
+                text[2].textContent = wrong_guess_message
+            }
+        }
+    }
+    
 }
 
 buttons[1].onclick = function() {
-    confirm()
+    confirm_guess() 
 }
+
+
